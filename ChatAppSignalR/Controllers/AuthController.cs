b9ad91b404d9eac62c1a444b745ddafbbf1a2b48
Lamquocviet <lamquocviet2005.nvt@gmail.com>
2026-03-30 18:12:29 +0700

    Add Api login register refresh logout'

---
+++ ChatAppSignalR/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using ChatAppSignalR.DTOs;
+using ChatAppSignalR.Models;
+using ChatAppSignalR.Services;
+
+
+
+
+namespace ChatAppSignalR.Controllers
+{
+    public class AuthController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class AuthController(IAuthService authService) : ControllerBase
+        {
+
+
+            [HttpPost("register")]
+            public async Task<ActionResult<object>> Register(RegisterRequest request)
+            {
+                var user = await authService.RegisterAsync(request);
+
+                if (user is null)
+                    return BadRequest("User already exists");
+
+                return Ok(new
+                {
+                    user.Username,
+                    user.Email
+                });
+            }
+
+            [HttpPost("login")]
+            public async Task<IActionResult> Login(LoginRequest request)
+            {
+                var result = await authService.LoginAsync(request);
+
+                if (result == null)
+                    return BadRequest("Invalid email or password!");
+
+                return Ok(new
+                {
+                    accessToken = result.Value.accessToken,
+                    refreshToken = result.Value.refreshToken
+                });
+            }
+            [HttpPost("refresh")]
+            public async Task<IActionResult> Refresh(RefreshRequest request)
+            {
+                var result = await authService.RefreshAsync(request.RefreshToken);
+
+                if (result == null)
+                    return BadRequest("Invalid refresh token");
+
+                return Ok(new
+                {
+                    accessToken = result.Value.accessToken,
+                    refreshToken = result.Value.refreshToken
+                });
+            }
+            [HttpPost("logout")]
+            public async Task<IActionResult> Logout(RefreshRequest request)
+            {
+                var success = await authService.LogoutAsync(request.RefreshToken);
+
+                if (!success)
+                    return BadRequest("Invalid refresh token");
+
+                return Ok("Logged out successfully");
+            }
+
+            [Authorize]
+            [HttpGet]
+            public IActionResult AuthenticatedOnlyEndpoint()
+            {
+                return Ok("You are authenticated!");
+            }
+
+        }
+    }
+}
---
+++ ChatAppSignalR/Data/MongoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ChatAppSignalR.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ChatAppSignalR.Settings;
+
+
+
+namespace ChatAppSignalR.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+        private readonly MongoDbSettings _settings;
+
+        public MongoDbContext(IOptions<MongoDbSettings> options)
+        {
+            _settings = options.Value;
+
+            var client = new MongoClient(_settings.ConnectionString);
+            _database = client.GetDatabase(_settings.DatabaseName);
+        }
+
+        public IMongoCollection<User> Users =>
+            _database.GetCollection<User>(_settings.UsersCollectionName);
+
+        public IMongoCollection<RefreshToken> RefreshTokens =>
+            _database.GetCollection<RefreshToken>(_settings.RefreshTokensCollectionName);
+    }
+}
