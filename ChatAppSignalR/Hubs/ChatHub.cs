c80fc88a42b478fcb2e0773f6d690a496b075fee
Binh05 <binhdt0452@gmail.com>
2026-04-02 21:15:13 +0700

    feat: add signalr realtime

--- ChatAppSignalR/Hubs/ChatHub.cs
+++ ChatAppSignalR/Hubs/ChatHub.cs
@@ -6,5 +6,28 @@ namespace ChatAppSignalR.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+    {
+        // Context.UserIdentifier sẽ lấy ID từ CustomUserIdProvider bạn đã đăng ký
+        var userId = Context.UserIdentifier;
+        var connectionId = Context.ConnectionId;
+
+        OnlineUsersStore.Add(userId!, connectionId);
+
+        await Clients.All.SendAsync("online-users", OnlineUsersStore.GetOnlineUsers());
+        
+        await base.OnConnectedAsync();
+    }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = Context.UserIdentifier;
+            var connectionId = Context.ConnectionId;
+
+            OnlineUsersStore.Remove(userId!, connectionId);
+
+            await Clients.All.SendAsync("online-user", OnlineUsersStore.GetOnlineUsers());
+
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
